3bce98ab0ef76d56f427abfe48219a68c17266d2
Amal Paul <amalpaul@Amals-MacBook-Air.local>
2023-05-03 02:39:06 +0530

    Remove additional apps

--- ci/clone-apps.sh
+++ ci/clone-apps.sh
@@ -1,7 +1,9 @@
 # !/bin/bash
 
-# Use PAT or ssh for private repos.
+# Create an empty repos directory to avoid error if no custom apps are installed
+mkdir repos
 
+# Use PAT or ssh for private repos.
 ## To use SSH keys place the private key as ssh-keys/ci and uncomment following
 # mkdir -p repos ~/.ssh
 # chown 0600 ssh-keys/ci
@@ -14,20 +16,16 @@
 # git clone --depth 1 --branch master ssh://git@github.com/frappe/twilio-integration repos/twilio_integration
 
 ## To use PAT use PERSONAL_ACCESS_TOKEN environment variable which is set through CI secret.
-# git clone --depth 1 --branch develop https://github.com/frappe/health repos/healthcare 
-git clone --depth 1 --branch ph-dev-wip https://pythonpen:$PYTHONPEN_GITHUB_PASSWORD@github.com/pythonpen/healthcare repos/healthcare
-
-git clone --depth 1 --branch main https://pythonpen:$PYTHONPEN_GITHUB_PASSWORD@github.com/pythonpen/tacten_core repos/tacten_core
+# git clone --depth 1 --branch ph-dev-wip https://pythonpen:$PYTHONPEN_GITHUB_PASSWORD@github.com/pythonpen/healthcare repos/healthcare
+# git clone --depth 1 --branch main https://pythonpen:$PYTHONPEN_GITHUB_PASSWORD@github.com/pythonpen/tacten_core repos/tacten_core
 
 # Following are public repositories.
-
-
 # git clone --depth 1 --branch develop https://github.com/frappe/health repos/healthcare
-git clone --depth 1 --branch develop https://github.com/frappe/payments repos/payments
-#git clone --depth 1 --branch master https://github.com/frappe/wiki repos/wiki
-git clone --depth 1 --branch version-14 https://github.com/resilient-tech/india-compliance repos/india_compliance
-git clone --depth 1 --branch version-14 https://github.com/frappe/hrms repos/hrms
-git clone --depth 1 --branch master https://github.com/Tacten/frappe-attachments-s3 repos/frappe_s3_attachment
-git clone --depth 1 --branch develop https://github.com/yrestom/POS-Awesome repos/posawesome
+# git clone --depth 1 --branch develop https://github.com/frappe/payments repos/payments
+# git clone --depth 1 --branch master https://github.com/frappe/wiki repos/wiki
+# git clone --depth 1 --branch version-14 https://github.com/resilient-tech/india-compliance repos/india_compliance
+# git clone --depth 1 --branch version-14 https://github.com/frappe/hrms repos/hrms
+# git clone --depth 1 --branch master https://github.com/Tacten/frappe-attachments-s3 repos/frappe_s3_attachment
+# git clone --depth 1 --branch develop https://github.com/yrestom/POS-Awesome repos/posawesome
 # git clone --depth 1 --branch main https://github.com/frappe/desk repos/frappedesk
-git clone --depth 1 --branch master https://github.com/frappe/frappe_theme.git repos/frappe_theme
+# git clone --depth 1 --branch master https://github.com/frappe/frappe_theme.git repos/frappe_theme
--- images/backend.Dockerfile
+++ images/backend.Dockerfile
@@ -6,15 +6,15 @@ FROM erpnext-worker
 COPY --chown=frappe:frappe repos ../apps
 
 USER root
-RUN install-app healthcare && \
+# RUN install-app healthcare && \
   # install-app wiki  && \
-  install-app india_compliance  && \
-  install-app tacten_core  && \
-  install-app payments && \
-  install-app frappe_s3_attachment && \
-  install-app hrms && \
-  install-app posawesome && \
-  install-app frappe_theme
+  # install-app india_compliance  && \
+  # install-app tacten_core  && \
+  # install-app payments && \
+  # install-app frappe_s3_attachment && \
+  # install-app hrms && \
+  # install-app posawesome && \
+  # install-app frappe_theme
 
 USER root
 RUN apt-get update && apt-get install -y libmagic-dev libmagic1 curl
